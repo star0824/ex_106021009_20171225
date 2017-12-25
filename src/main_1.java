47e4f00b0681f2501f4efd36b73d0e6ef8bc967a
star0824 <star880824@gmail.com>
2017-12-25 16:04:47 +0800

    ex_106021009_20171225
    ex_106021009_20171225

---
+++ src/main_1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class main_1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int count[] = new int[52];
+        String str = scn.nextLine();
+        String[] data = str.split(" ");
+        int n = data.length;
+        System.out.println(n);
+        int leng = str.length();
+        for (int i=0 ; i<leng ; i++) {
+            int val = str.charAt(i) - 'a';
+            if (val >= 0 && val < 26) {
+                count[val]++;
+            }
+            else {
+                val = str.charAt(i) - 'A';
+                if (val >= 0 && val < 26) {
+                    count[26 + val]++;
+                }
+            }
+        }
+        for (int i=0 ; i<52 ; i++) {
+            if (count[i] > 0) {
+                if (i < 26) {
+                    System.out.println((char) (97 + i) + ":" + count[i]);
+                } else {
+                    System.out.println((char) (65 + i) + ":" + count[i]);
+                }
+            }
+        }
+    }
+}
